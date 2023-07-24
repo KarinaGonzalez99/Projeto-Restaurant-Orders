871a57835942307bc9334bd1c49a95691bc9c53f
Karina Gonzalez <karinagonzalez99@gmail.com>
2023-07-24 16:18:42 -0300

    Requisito 4, erro test_get_main_menu_returns_no_dishes_when_ingredients_are_not_available

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -1,7 +1,7 @@
 from typing import Dict, List
-
 from services.inventory_control import InventoryMapping
 from services.menu_data import MenuData
+from src.models.ingredient import restriction_map
 
 DATA_PATH = "data/menu_base_data.csv"
 INVENTORY_PATH = "data/inventory_base_data.csv"
@@ -24,6 +24,21 @@ class MenuBuilder:
 
         self.inventory.consume_recipe(curr_dish.recipe)
 
-    # Req 4
-    def get_main_menu(self, restriction=None) -> List[Dict]:
-        pass
+    def get_main_menu(self, restriction: restriction_map = None) -> List[Dict]:
+        result = []
+        for dish in self.menu_data.dishes:
+            # Verifica se não há restrição não está presente prato
+            if (
+                restriction is None
+                or restriction not in dish.get_restrictions()
+            ):
+                dish_info = {
+                    "dish_name": dish.name,
+                    "ingredients": list(dish.recipe.keys()),
+                    "price": dish.price,
+                    "restrictions": dish.get_restrictions(),
+                }
+                result.append(dish_info)
+        return result
+
+        # Monta o dicionário e adiciona na lista de resultadoo
